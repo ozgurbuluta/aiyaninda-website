85d15a7596db9d906be321159c893dd77b88c869
Özgür Bulut Akanay <ozgur@Ozgurs-MBP.fritz.box>
2024-11-24 10:07:26 +0100

    vercel speed analytics added

--- app/layout.js
+++ app/layout.js
@@ -47,6 +47,7 @@ export default function RootLayout({ children }) {
             onClose={() => setShowContactModal(false)}
           />
         )}
+        <SpeedInsights />
       </body>
     </html>
   );
